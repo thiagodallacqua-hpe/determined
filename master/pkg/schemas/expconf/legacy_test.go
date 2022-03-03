971ad19ff27fe1d57188fdf67f22243c31a56e72
Ilia Glazkov <ilia@hpe.com>
2022-03-02 18:57:43 -0800

    update apex

--- master/pkg/schemas/expconf/legacy_test.go
+++ master/pkg/schemas/expconf/legacy_test.go
@@ -257,8 +257,8 @@ func TestLegacyConfig(t *testing.T) {
                     gpu: []
                   force_pull_image: false
                   image:
-                    cpu: determinedai/environments-dev:py-3.7-pytorch-1.7-tf-1.15-cpu-136a60e
-                    gpu: determinedai/environments-dev:cuda-10.2-pytorch-1.7-tf-1.15-gpu-136a60e
+                    cpu: determinedai/environments-dev:py-3.7-pytorch-1.7-tf-1.15-cpu-ca75ad6
+                    gpu: determinedai/environments-dev:cuda-10.2-pytorch-1.7-tf-1.15-gpu-ca75ad6
                   pod_spec:
                     apiVersion: v1
                     kind: Pod
